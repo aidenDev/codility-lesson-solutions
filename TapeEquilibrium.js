154e2a2fb3aec75e624622db2b4e9449b160baee
aidenDev <aidevrell@gmail.com>
2019-07-30 00:42:13 +0100

    [Fix] Tidy up solution

--- TapeEquilibrium.js
+++ TapeEquilibrium.js
@@ -1,13 +1,7 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
-function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    
+function solution(A) { // O(N)
     let maxSecondPartSum = getMaxSecondPartSum(A);
     let minDiff = Number.MAX_VALUE;
-    
-    var firstPartSum = 0
+    let firstPartSum = 0;
     
     for (i = 0; i < A.length-1; i++) {
         firstPartSum += A[i];
