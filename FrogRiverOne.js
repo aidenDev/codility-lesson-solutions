fba07661be6170cfb9ab3c86e63f8d5fd2708b7b
aidenDev <aidevrell@gmail.com>
2019-07-29 23:16:06 +0100

    [Feat] Add solution to FrogRiverOne

---
+++ FrogRiverOne.js
@@ -0,0 +1,24 @@
+function solution(X, A) {  // O(N)
+    let leavesRequired = constructLeavesRequiredSet(X);
+    
+    for (i = 0; i < A.length; i++) { 
+        leavesRequired.delete(A[i]);
+
+        if (leavesRequired.size === 0) {
+            return i; 
+        }
+    }
+    
+    return -1;
+}
+
+
+function constructLeavesRequiredSet(X) {
+    let leavesRequired = new Set();
+    
+    for (i = 1; i <= X; i++) {
+        leavesRequired.add(i);
+    }
+    
+    return leavesRequired;
+}
