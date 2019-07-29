b71dabea41dc46ce956dd4830cae77f22111f608
aidenDev <aidevrell@gmail.com>
2019-07-30 00:20:54 +0100

    [Fix] Tidy up solution

--- task-2.js
+++ task-2.js
@@ -1,24 +1,13 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
 function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
+    sortedArr = A.sort();
     
-    if (A.length == 1) {
-        return A[0]
-    }
-    
-    sortedArr = A.sort()
-    
-    for (i=0; i<sortedArr.length; i=i+2) {
+    for (i = 0; i < sortedArr.length; i = i+2) {
 
-        // at last element
-        if (i+1 >= sortedArr.length) {
+        if (sortedArr[i] != sortedArr[i+1]) {
             return sortedArr[i]
         }
-
         
-        if (sortedArr[i] != sortedArr[i+1]) {
+        if (i+1 >= sortedArr.length) { // at last element
             return sortedArr[i]
         }
     }
